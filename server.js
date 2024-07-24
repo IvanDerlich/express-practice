c8b0a53b1520db7fe6b8a85e85276ffb81c68954
Ivan Derlich <a@ivanderlich.com>
2024-07-24 17:17:30 -0300

    More routes

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.listen(4564);
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", { texteue: "Hello World" });
+});
+
+app.get("/users", (req, res) => {
+  res.send("User List");
+});
+
+app.get("/users/new", (req, res) => {
+  res.send("User Form");
 });
