800447cc806d3356e976b4972c681309d4eb64a6
Ivan Derlich <a@ivanderlich.com>
2024-07-24 18:12:41 -0300

    Logger

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,26 @@ router.get("/new", (req, res) => {
   res.send("User Form");
 });
 
+router.post("/", (req, res) => {
+  res.send("User Created");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send("Get User id: " + req.params.id);
+  })
+  .put((req, res) => {
+    res.send("Update user with id: " + req.params.id);
+  })
+  .delete((req, res) => {
+    res.send("Delete user with id: " + req.params.id);
+  });
+
+router.param("id", (req, res, next, id) => {
+  console.log("id", id);
+  req.params.id = Number(id) + 999;
+  next();
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -6,10 +6,17 @@ app.listen(4564);
 
 app.set("view engine", "ejs");
 
+app.use(logger);
+
 app.get("/", (req, res) => {
   res.render("index", { texteue: "Hello World" });
 });
 
 const userRouter = require("./routes/users");
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 app.use("/users", userRouter);
